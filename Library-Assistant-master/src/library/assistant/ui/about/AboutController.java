eda564651493d73b26e1014125c427215fb140ae
Kennedy <argosax2@yahoo.com>
2020-05-15 20:59:28 -0400

    Admin-like Library Assistant Desktop Application.

---
+++ Library-Assistant-master/src/library/assistant/ui/about/AboutController.java
@@ -0,0 +1,72 @@
+package library.assistant.ui.about;
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
+import library.assistant.alert.AlertMaker;
+import library.assistant.util.LibraryAssistantUtil;
+
+public class AboutController implements Initializable {
+
+    private static final String LINKED_IN = "https://www.linkedin.com/in/kennedynwoke";
+    private static final String FACEBOOK = "http://facebook.com/afsalashyana";
+    private static final String WEBSITE = "";
+    private static final String GITHUB = "https://github.com/EliteKenn";
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        AlertMaker.showTrayMessage(String.format("Hello %s!", System.getProperty("user.name")), "Thanks for trying out Library Assistant");
+    }
+
+    private void loadWebpage(String url) {
+        try {
+            Desktop.getDesktop().browse(new URI(url));
+        } catch (IOException | URISyntaxException e1) {
+            e1.printStackTrace();
+            handleWebpageLoadException(url);
+        }
+    }
+
+    private void handleWebpageLoadException(String url) {
+        WebView browser = new WebView();
+        WebEngine webEngine = browser.getEngine();
+        webEngine.load(url);
+        Stage stage = new Stage();
+        Scene scene = new Scene(new StackPane(browser));
+        stage.setScene(scene);
+        stage.setTitle("EliteKenn");
+        stage.show();
+        LibraryAssistantUtil.setStageIcon(stage);
+    }
+
+    @FXML
+    private void loadGithubChannel(ActionEvent event) {
+        loadWebpage(GITHUB);
+    }
+
+    @FXML
+    private void loadBlog(ActionEvent event) {
+        loadWebpage(WEBSITE);
+    }
+
+    @FXML
+    private void loadLinkedIN(ActionEvent event) {
+        loadWebpage(LINKED_IN);
+    }
+
+    @FXML
+    private void loadFacebook(ActionEvent event) {
+        loadWebpage(FACEBOOK);
+    }
+}
